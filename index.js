3fce12bc60b6000b5b4ab0c2f75e1fcd4efdacaf
Ahashanahmed2 <ahashanahmed@yahoo.com>
2021-12-12 19:53:06 +0800

    ss

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ env.config();
 http.createServer(app)
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'images')))
+app.use('/images',express.static(path.join(__dirname,'images')))
 app.set('view engine','ejs')
 //database connected
 mongoose.connect(process.env.DB)
